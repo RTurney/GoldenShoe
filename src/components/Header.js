f46483ddf6fc3302c1aed6b35f40c3a1af56f803
RTurney <r.turney93@gmail.com>
2021-04-21 16:08:23 +0100

    Add Golden shoe logo. Add home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import Navbar from './Navbar';
 import SearchBar from './SearchBar';
 import React, { Component } from 'react';
+import logo from './GoldenShoe.png'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 class Header extends Component {
     render() {
         return (
             <div className='HeaderContainer'>
-                <h1>Golden Shoe</h1>
+                <Link to='/' className='logo-section'>
+                    <img src={logo} alt='Golden Shoe' className='logo'/>
+                </Link>
                 <Navbar />
                 <SearchBar />
             </div>
